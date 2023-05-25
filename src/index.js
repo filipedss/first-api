f9f294349c41b72281062c6b8c2121010514463d
Filipe da Silva Santos <filipssant@gmail.com>
2023-05-25 19:56:42 -0300

    add route file

--- src/index.js
+++ src/index.js
@@ -1,18 +1,13 @@
 const http = require('http');
-const UserController = require('./controllers/UserController');
 
 const server = http.createServer((request, response) => {
-
   console.log(`Request method: ${request.method} | Endpoint: ${request.url}`);
-
   if(request.url === '/users' && request.method === 'GET') {
     UserController.listUsers(request, response);
   }else{
     response.writeHead(404, {'Content-Type': 'text/html' });
     response.end(`Cannot ${request.method} ${request.url}`);
   }
-
-  
 });
 
 server .listen(3000, () => console.log('live'));
