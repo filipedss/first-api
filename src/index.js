5848d7106a0373c5e70132fc4925513abbf24bf6
Filipe da Silva Santos <filipssant@gmail.com>
2023-05-25 16:21:02 -0300

    Import users data

--- src/index.js
+++ src/index.js
@@ -1,11 +1,16 @@
 const http = require('http');
+const users = require('./mocks/users');
 
 const server = http.createServer((request, response) => {
-    
+
     console.log(`Request method: ${request.method} | Endpoint: ${request.url}`);
 
-    response.writeHead(200, { 'Content-Type' : 'text/html'});
-    response.end('<h1>Hello World1</h1>')
+    if(request.url === '/users' && request.method === 'GET') {
+        response.writeHead(200, { 'Content-Type' : 'application/json'});
+        response.end('<h1>Hello World1</h1>')
+    }
+
+    
 });
 
 server .listen(3000, () => console.log('live'));
