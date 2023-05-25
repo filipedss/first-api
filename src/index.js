def0a2232ee69cf12a5d11e8678f053d467c5c2c
Filipe da Silva Santos <filipssant@gmail.com>
2023-05-25 15:53:43 -0300

    up server on port 3000

--- src/index.js
+++ src/index.js
@@ -3,4 +3,6 @@ const http = require('http');
 const server = http.createServer((request, response) => {
     response.writeHead(200, { 'Content-Type' : 'text/html'});
     response.end('<h1>Hello World</h1>')
-})
+});
+
+server .listen(3000, () => console.log('live'));
