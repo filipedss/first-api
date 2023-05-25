1b73f482dc22f761c66084bc0bb71901f327f07a
Filipe da Silva Santos <filipssant@gmail.com>
2023-05-25 16:08:53 -0300

    show request endpoint and method

--- src/index.js
+++ src/index.js
@@ -1,8 +1,11 @@
 const http = require('http');
 
 const server = http.createServer((request, response) => {
+    
+    console.log(`Request method: ${request.method} | Endpoint: ${request.url}`);
+
     response.writeHead(200, { 'Content-Type' : 'text/html'});
-    response.end('<h1>Hello World</h1>')
+    response.end('<h1>Hello World1</h1>')
 });
 
 server .listen(3000, () => console.log('live'));
