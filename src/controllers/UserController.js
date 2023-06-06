9405430c5164d5f17e88162ec287391e9b261e07
Filipe da Silva Santos <filipssant@gmail.com>
2023-06-06 18:02:49 -0300

    function to add new user

--- src/controllers/UserController.js
+++ src/controllers/UserController.js
@@ -20,5 +20,21 @@ module.exports = {
     }else{
       response.send(200, user);
     }
+  },
+  createUser(request, response) {
+    let body = '';
+    request.on('data', (chunk) => {
+      body+=chunk;
+    });
+    request.on('end', () => {
+      body = JSON.parse(body);
+      const lastUserId = users[users.length-1].id;
+      const newUser = {
+        id: lastUserId + 1,
+        name: body.name,
+      };
+      users.push(newUser);
+      response.send(200, newUser);
+    });
   }
 };
