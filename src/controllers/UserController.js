3781d0768a69c430bef7489da27826815751ca31
Filipe Santos <filipssant@gmail.com>
2023-06-26 20:50:13 -0300

    delete function

--- src/controllers/UserController.js
+++ src/controllers/UserController.js
@@ -35,15 +35,11 @@ module.exports = {
   updateUser(request, response){
     let { id } = request.params;
     const { name } = request.body;
-
     id = Number(id);
-
     const userExists = users.find((user) => user.id === id);
-
     if(!userExists){
       return response.send(400, {error: 'User not found'})
     }
-
     users = users.map((user) => {
       if(user.id === id) {
         return {
@@ -51,10 +47,15 @@ module.exports = {
           name,
         };
       }
-
       return user;
     });
-
     response.send(200, { id, name});
+  },
+  deleteUser(request, response) {
+    let { id } = request.params;
+    id = Number(id);
+
+    users = users.filter((user) => user.id !== id);
+    response.send(200, {deleted: true});
   }
 };
